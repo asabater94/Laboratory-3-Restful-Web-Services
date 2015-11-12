afc5071f8c02588b82aa0cbea13a28d074264a90
Alberto Sabater <asabater94@gmail.com>
2015-11-12 18:46:35 +0100

    Test updated

--- src/test/java/rest/addressbook/AddressBookServiceTest.java
+++ src/test/java/rest/addressbook/AddressBookServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -23,7 +24,10 @@ import rest.addressbook.Person;
 
 /**
  * A simple test suite
- *
+ * 
+ * safe:  the server's status is the same as before
+ * Indempotent:	there is no new information in the server after repeat the 
+ * same request
  */
 public class AddressBookServiceTest {
 
@@ -31,6 +35,7 @@ public class AddressBookServiceTest {
 
 	@Test
 	public void serviceIsAlive() throws IOException {
+		System.out.println("serviceIsAlive()");
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		launchServer(ab);
@@ -40,17 +45,33 @@ public class AddressBookServiceTest {
 		Response response = client.target("http://localhost:8282/contacts")
 				.request().get();
 		assertEquals(200, response.getStatus());
-		assertEquals(0, response.readEntity(AddressBook.class).getPersonList()
-				.size());
+		AddressBook ab1 = response.readEntity(AddressBook.class);
+		assertEquals(0, ab1.getPersonList().size());
 
 		//////////////////////////////////////////////////////////////////////
 		// Verify that GET /contacts is well implemented by the service, i.e
 		// test that it is safe and idempotent
 		//////////////////////////////////////////////////////////////////////	
+		
+		Response response2 = client.target("http://localhost:8282/contacts")
+				.request().get();
+		assertEquals(200, response2.getStatus());
+		AddressBook ab2 = response2.readEntity(AddressBook.class);
+		
+		assertEquals(ab1.getNextId(), ab2.getNextId());
+		assertEquals(ab1.getPersonList().size(), ab2.getPersonList().size());
+		
+		System.out.println();
+		
+		// As the result is the same, we can say that GET /contacts it's 
+		// idempotent and safe because the server's status is the same
 	}
 
 	@Test
 	public void createUser() throws IOException {
+		
+		System.out.println("createUser()");
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		launchServer(ab);
@@ -69,6 +90,7 @@ public class AddressBookServiceTest {
 		assertEquals(201, response.getStatus());
 		assertEquals(juanURI, response.getLocation());
 		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		// Compare response person with the person posted
 		Person juanUpdated = response.readEntity(Person.class);
 		assertEquals(juan.getName(), juanUpdated.getName());
 		assertEquals(1, juanUpdated.getId());
@@ -88,11 +110,47 @@ public class AddressBookServiceTest {
 		// Verify that POST /contacts is well implemented by the service, i.e
 		// test that it is not safe and not idempotent
 		//////////////////////////////////////////////////////////////////////	
+		
+		
+		response = client.target("http://localhost:8282/contacts")
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(juan, MediaType.APPLICATION_JSON));
+
+		assertEquals(201, response.getStatus());
+		assertNotEquals(juanURI, response.getLocation());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		juanUpdated = response.readEntity(Person.class);
+		assertEquals(juan.getName(), juanUpdated.getName());
+		assertNotEquals(1, juanUpdated.getId());
+		assertNotEquals(juanURI, juanUpdated.getHref());
 				
+		// POST /contacts it's not safe because executing the same operation, the 
+		// result is not the same.
+		
+		
+		// Check that the new user exists
+		response = client.target("http://localhost:8282/contacts/person/2")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		juanUpdated = response.readEntity(Person.class);
+		assertEquals(juan.getName(), juanUpdated.getName());
+		assertNotEquals(1, juanUpdated.getId());
+		assertNotEquals(juanURI, juanUpdated.getHref());
+		System.out.println("Juan: " + juanURI);
+		
+		// POST /contacts it's not idempotent because after two same post there are
+		// two new persons with different ids
+		
+		System.out.println();
+		
 	}
 
 	@Test
 	public void createUsers() throws IOException {
+		
+		System.out.println("createUsers()");
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		Person salvador = new Person();
@@ -138,16 +196,60 @@ public class AddressBookServiceTest {
 		assertEquals(maria.getName(), mariaUpdated.getName());
 		assertEquals(3, mariaUpdated.getId());
 		assertEquals(mariaURI, mariaUpdated.getHref());
-
+		
+		// Gets the address book
+		response = client.target("http://localhost:8282/contacts")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		AddressBook ab1 = response.readEntity(AddressBook.class);
+				
 		//////////////////////////////////////////////////////////////////////
 		// Verify that GET /contacts/person/3 is well implemented by the service, i.e
 		// test that it is safe and idempotent
 		//////////////////////////////////////////////////////////////////////	
 	
+		// Check that the new user exists
+		response = client.target("http://localhost:8282/contacts/person/3")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		Person mariaUpdated2 = response.readEntity(Person.class);
+		assertEquals(mariaUpdated2.getName(), mariaUpdated.getName());
+		assertEquals(mariaUpdated2.getId(), mariaUpdated.getId());
+		assertEquals(mariaUpdated2.getHref(), mariaUpdated.getHref());
+		
+		// GET /contacts/person/3 it's idempotent because after repeat this operation
+		// twice, the person obtained it's the same
+		
+		// Gets the address book again
+		response = client.target("http://localhost:8282/contacts")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		AddressBook ab2 = response.readEntity(AddressBook.class);
+		
+		ArrayList<Person> list1 = (ArrayList<Person>) ab1.getPersonList();
+		ArrayList<Person> list2 = (ArrayList<Person>) ab2.getPersonList();
+		assertEquals(list1.size(), list2.size());
+		for (int i=0; i<list1.size(); i++) {
+			assertEquals(list1.get(i).getId(), list2.get(i).getId());
+			assertEquals(list1.get(i).getHref(), list2.get(i).getHref());
+			assertEquals(list1.get(i).getName(), list2.get(i).getName());
+			assertEquals(list1.get(i).getEmail(), list2.get(i).getEmail());
+		}
+		
+		// GET /contacts/person/3 it's safe because after repeat this operation twice
+		// the are no new information in the server
+		
+		System.out.println();
+		
 	}
 
 	@Test
 	public void listUsers() throws IOException {
+		
+		System.out.println("listUsers()");
 
 		// Prepare server
 		AddressBook ab = new AddressBook();
@@ -175,11 +277,20 @@ public class AddressBookServiceTest {
 		// Verify that POST is well implemented by the service, i.e
 		// test that it is not safe and not idempotent
 		//////////////////////////////////////////////////////////////////////	
+		
+		// POST it isn't idempotent because it adds new information to the server
+		// and it isn't safe, because the result of two same posts its different
+		// as I've proved before
+		
+		System.out.println();
 	
 	}
 
 	@Test
 	public void updateUsers() throws IOException {
+		
+		System.out.println("createusers()");
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		Person salvador = new Person();
@@ -228,11 +339,32 @@ public class AddressBookServiceTest {
 		// Verify that PUT /contacts/person/2 is well implemented by the service, i.e
 		// test that it is idempotent
 		//////////////////////////////////////////////////////////////////////	
-	
+		
+		// Update Maria again
+		response = client
+				.target("http://localhost:8282/contacts/person/2")
+				.request(MediaType.APPLICATION_JSON)
+				.put(Entity.entity(maria, MediaType.APPLICATION_JSON));
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		Person mariaRetrieved2 = response.readEntity(Person.class);
+		assertEquals(mariaRetrieved2.getId(), mariaRetrieved2.getId());
+		assertEquals(mariaRetrieved2.getHref(), mariaRetrieved2.getHref());
+		assertEquals(mariaRetrieved2.getName(), mariaRetrieved2.getName());
+		assertEquals(mariaRetrieved2.getEmail(), mariaRetrieved2.getEmail());
+
+		// PUT it's idempotent because after executing the same PUT twice, the
+		// server's status has no new information
+
+		System.out.println();
+		
 	}
 
 	@Test
 	public void deleteUsers() throws IOException {
+		
+		System.out.println("deleteUsers()");
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		Person salvador = new Person();
@@ -251,21 +383,41 @@ public class AddressBookServiceTest {
 				.target("http://localhost:8282/contacts/person/2").request()
 				.delete();
 		assertEquals(204, response.getStatus());
+		
+		// Verify that the update is real
+		response = client.target("http://localhost:8282/contacts/person/2")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(404, response.getStatus());
+		Person personRetrieved = response.readEntity(Person.class);
 
 		// Verify that the user has been deleted
 		response = client.target("http://localhost:8282/contacts/person/2")
 				.request().delete();
 		assertEquals(404, response.getStatus());
+		
+		// Verify that the update is real
+		response = client.target("http://localhost:8282/contacts/person/2")
+				.request(MediaType.APPLICATION_JSON).get();
+		assertEquals(404, response.getStatus());
+		Person personRetrieved2 = response.readEntity(Person.class);
 
 		//////////////////////////////////////////////////////////////////////
 		// Verify that DELETE /contacts/person/2 is well implemented by the service, i.e
 		// test that it is idempotent
 		//////////////////////////////////////////////////////////////////////	
+		
+		// DELETE it's idempotent because after executing this operatin twice,
+		// the server's status it's the same
+		
+		System.out.println();
 
 	}
 
 	@Test
 	public void findUsers() throws IOException {
+		
+		System.out.println("findUsers()");
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		Person salvador = new Person();
@@ -304,6 +456,9 @@ public class AddressBookServiceTest {
 		response = client.target("http://localhost:8282/contacts/person/3")
 				.request(MediaType.APPLICATION_JSON).get();
 		assertEquals(404, response.getStatus());
+		
+		
+		System.out.println();
 	}
 
 	private void launchServer(AddressBook ab) throws IOException {
